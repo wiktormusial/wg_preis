b526933d0ab7225dc98950db129f708f535defdb
Wiktor Musial <wiktormusial@icloud.com>
2022-05-24 21:11:54 +0200

    Add corsheaders

--- wg_preis/settings.py
+++ wg_preis/settings.py
@@ -42,17 +42,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'cities',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    os.environ['FRONTEND_LINK'],
 ]
 
 ROOT_URLCONF = 'wg_preis.urls'
