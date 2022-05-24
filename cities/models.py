5179016f571f62b5ab8147eb28179f7109e1a4b9
Wiktor Musial <wiktormusial@icloud.com>
2022-05-24 20:22:07 +0200

    Create models

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    city = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.city
+
+class Price(models.Model):
+    city = models.ForeignKey(City,
+                             on_delete=models.CASCADE)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
