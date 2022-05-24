adde5b8efa036a04146b8293ff3492404e0f5924
Wiktor Musial <wiktormusial@icloud.com>
2022-05-24 21:07:09 +0200

    Create CitiesListView

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CitySerializer
+from .models import City
 
-# Create your views here.
+
+class CitiesListView(generics.ListAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
