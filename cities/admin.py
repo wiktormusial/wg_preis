64462cf16c157bdeb17875404a4887f28c06d0fe
Wiktor Musial <wiktormusial@icloud.com>
2022-05-24 20:28:57 +0200

    Create slugfield in city

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Price, City
 
 # Register your models here.
+admin.site.register(Price)
+admin.site.register(City)
--- cities/models.py
+++ cities/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class City(models.Model):
     city = models.CharField(max_length=30)
+    slug = models.SlugField(null=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.city
+
+    def __save__(self):
+        if not self.slug:
+            self.slug = slugify(self.city)
+        return super().save(*args, **kwargs)
+
 
 class Price(models.Model):
     city = models.ForeignKey(City,
