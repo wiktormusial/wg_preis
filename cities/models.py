4bb0128b4f47622cf154b71abb57bf7b1dc7aa2a
Wiktor Musial <wiktormusial@icloud.com>
2022-05-26 16:03:24 +0200

    Add isFav field to City model

--- cities/models.py
+++ cities/models.py
@@ -7,6 +7,7 @@ class City(models.Model):
     slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    isFav = models.BooleanField(default=False)
 
     def __str__(self):
         return self.city
