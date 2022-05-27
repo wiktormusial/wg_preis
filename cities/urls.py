5fcfa5b4ecbf04e32acaae319cae0b925c0fc109
Wiktor Musial <wiktormusial@icloud.com>
2022-05-27 22:23:47 +0200

    Create City Detail veiw

--- cities/urls.py
+++ cities/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CitiesListView, CitiesFavListView
+from .views import CitiesListView, CitiesFavListView, CitiesDetailView
 
 urlpatterns = [
     path('getcities/', CitiesListView.as_view()),
+    path('getcity/<slug:slug>/', CitiesDetailView.as_view()),
     path('getfavcities/', CitiesFavListView.as_view())
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import CitySerializer
 from .models import City
 
@@ -10,3 +13,15 @@ class CitiesListView(generics.ListAPIView):
 class CitiesFavListView(generics.ListAPIView):
     queryset = City.objects.filter(isFav=True)
     serializer_class = CitySerializer
+
+class CitiesDetailView(APIView):
+    def get_object(self, slug):
+        try:
+            return City.objects.get(slug=slug)
+        except City.DoesNotExist:
+            raise Http404
+
+    def get(self, request, slug, format=None):
+        city = self.get_object(slug)
+        serializer = CitySerializer(city)
+        return Response(serializer.data)
